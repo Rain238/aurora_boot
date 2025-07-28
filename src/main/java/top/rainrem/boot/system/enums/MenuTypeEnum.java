3f6fafd992256a6a10a5bb1b9d73168625fb5952
LightRain <3164395730@qq.com>
2025-07-28 20:54:01 +0800

    菜单类型枚举

---
+++ src/main/java/top/rainrem/boot/system/enums/MenuTypeEnum.java
@@ -0,0 +1,34 @@
+package top.rainrem.boot.system.enums;
+
+import com.baomidou.mybatisplus.annotation.EnumValue;
+import lombok.Getter;
+import top.rainrem.boot.common.base.IBaseEnum;
+
+/**
+ * 菜单类型枚举
+ *
+ * @author LightRain
+ * @since 2025年7月23日19:51:44
+ */
+@Getter
+public enum MenuTypeEnum implements IBaseEnum<Integer> {
+
+    NULL(0, null),
+    MENU(1, "菜单"),
+    CATALOG(2, "目录"),
+    EXTLINK(3, "外链"),
+    BUTTON(4, "按钮");
+
+    //  Mybatis-Plus 提供注解表示插入数据库时插入该值
+    @EnumValue
+    private final Integer value;
+
+    // @JsonValue //  表示对枚举序列化时返回此字段
+    private final String label;
+
+    MenuTypeEnum(Integer value, String label) {
+        this.value = value;
+        this.label = label;
+    }
+
+}
