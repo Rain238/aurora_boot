a0d51f47735ad951e468225ddffcd8093eca32e4
LightRain <3164395730@qq.com>
2025-07-28 20:23:28 +0800

    响应码接口

---
+++ src/main/java/top/rainrem/boot/common/result/IResultCode.java
@@ -0,0 +1,11 @@
+package top.rainrem.boot.common.result;
+
+/**
+ * 响应码接口
+ * @author LightRain
+ * @since 2025/7/22
+ */
+public interface IResultCode {
+    String getCode();
+    String getMsg();
+}
