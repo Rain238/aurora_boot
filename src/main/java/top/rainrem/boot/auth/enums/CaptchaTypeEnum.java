57be19cbd5c18d6dc393b169e8ce44cced979088
LightRain <3164395730@qq.com>
2025-07-28 19:45:38 +0800

    验证码类型枚举

---
+++ src/main/java/top/rainrem/boot/auth/enums/CaptchaTypeEnum.java
@@ -0,0 +1,27 @@
+package top.rainrem.boot.auth.enums;
+
+/**
+ * EasyCaptcha 验证码类型枚举
+ *
+ * @author LightRain
+ * @since 2025年7月22日20:35:55
+ */
+public enum CaptchaTypeEnum {
+
+    /**
+     * 圆圈干扰验证码
+     */
+    CIRCLE,
+    /**
+     * GIF验证码
+     */
+    GIF,
+    /**
+     * 干扰线验证码
+     */
+    LINE,
+    /**
+     * 扭曲干扰验证码
+     */
+    SHEAR
+}
