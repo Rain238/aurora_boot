2a7f1572a3f68e95973d7b653a6419d53cec3fb9
LightRain <3164395730@qq.com>
2025-07-29 20:31:30 +0800

    用户认证凭证信息

---
+++ src/main/java/top/rainrem/boot/core/security/model/UserAuthCredentials.java
@@ -0,0 +1,54 @@
+package top.rainrem.boot.core.security.model;
+
+import lombok.Data;
+
+import java.util.Set;
+
+/**
+ * 用户认证凭证信息
+ *
+ * @author LightRain
+ * @since 2025年7月22日18:06:01
+ */
+@Data
+public class UserAuthCredentials {
+
+
+    /**
+     * 用户ID
+     */
+    private Long userId;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 昵称
+     */
+    private String nickname;
+
+    /**
+     * 用户密码
+     */
+    private String password;
+
+    /**
+     * 状态（1:启用；0:禁用）
+     */
+    private Integer status;
+
+    /**
+     * 用户所属的角色集合
+     */
+    private Set<String> roles;
+
+    /**
+     * 数据权限范围，用于控制用户可以访问的数据级别
+     *
+     * @see com.youlai.boot.common.enums.DataScopeEnum
+     */
+    private Integer dataScope;
+
+}
