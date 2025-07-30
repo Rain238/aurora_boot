c3315563c8d47ef442d2111c94f2e533ef1bb067
LightRain <3164395730@qq.com>
2025-07-30 18:29:21 +0800

    caffeine缓存配置

---
+++ src/main/java/top/rainrem/boot/config/CaffeineConfig.java
@@ -0,0 +1,37 @@
+package top.rainrem.boot.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * caffeine缓存配置
+ *
+ * @author LightRain
+ * @since 2025年7月30日16:48:05
+ */
+@Slf4j
+@Configuration
+public class CaffeineConfig {
+
+    @Value("${spring.cache.caffeine.spec}")
+    private String caffeineSpec;
+
+    /**
+     * 缓存管理器
+     *
+     * @return CacheManager 缓存管理器
+     */
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        Caffeine<Object, Object> caffeineBuilder = Caffeine.from(caffeineSpec);
+        caffeineCacheManager.setCaffeine(caffeineBuilder);
+        return caffeineCacheManager;
+    }
+}
+
