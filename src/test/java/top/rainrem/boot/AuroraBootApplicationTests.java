3e854f3dffedebca43642065322499855a63beaa
LightRain <3164395730@qq.com>
2025-07-30 18:39:01 +0800

    AuroraBootApplicationTests.java

---
+++ src/test/java/top/rainrem/boot/AuroraBootApplicationTests.java
@@ -0,0 +1,13 @@
+package top.rainrem.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuroraBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
